40ca5f4013a7039dee76fbd9ec0314fdeeb3ebcc
rnbsrva <orynbasarovaakerke88@gmail.com>
2024-07-26 17:35:48 +0500

    pgsync config

--- src/main/java/com/akerke/ecommerceapi/elasticsearch/ProductSearch.java
+++ src/main/java/com/akerke/ecommerceapi/elasticsearch/ProductSearch.java
@@ -1,11 +1,15 @@
 package com.akerke.ecommerceapi.elasticsearch;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
 
 @Document(indexName = "product")
+@Getter
+@Setter
 public class ProductSearch {
 
     @Id
